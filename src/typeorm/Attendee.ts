4921d118f22df25c57d6a41d63ec7891e5667ba4
Lidiane Nunes <lidiane2002nunes@gmail.com>
2025-08-25 17:08:33 -0100

    Criei attendee module, controler e service, fiz tambem o typeorm do attendee

---
+++ src/typeorm/Attendee.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class Attendee {
+    @PrimaryGeneratedColumn()
+    id :number
+     
+    @Column()
+userId :string; // (FK → User)
+
+ @Column()
+eventId :string //(FK → Event)
+
+ @Column()
+status:string; // ('going' | 'interested' | 'declined')
+
+ @CreateDateColumn()
+createdAt :number;
+}
---
+++ src/typeorm/index.ts
@@ -0,0 +1,7 @@
+import { User } from "./User";
+import { Event } from "./Event";
+
+export const entities = [
+    User,
+    Event
+]
