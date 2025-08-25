ce6bd6096de4b2d60de073ac3d53cc8095f36aa5
Lidiane Nunes <lidiane2002nunes@gmail.com>
2025-08-25 15:34:43 -0100

    I created an app module and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/User';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "root",
+    database: "user_db",
+    entities: [User],
+    synchronize: true,
+    
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports:[TypeOrmModule.forRoot({
-    type:"postgres",
-    host:"localhost",
-    port:5432,
-    username:"postgres",
-    password:"123456",
-    database:"user_db",
-    entities:[],
-    synchronize:true,
-  })
-  
-  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
