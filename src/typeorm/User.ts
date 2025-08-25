d802a485153d5d8749112b91f5ab6b3b09ccfc00
Lidiane Nunes <lidiane2002nunes@gmail.com>
2025-08-25 15:54:05 -0100

    Add propriedade de users

--- src/typeorm/User.ts
+++ src/typeorm/User.ts
@@ -1,17 +1,23 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn  } from "typeorm";
 
 @Entity()
 export class User {
+
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column()
     name: string;
+
     @Column()
     email: string;
+
     @Column()
     password: string;
+
     @Column()
     role: string;
+
     @Column()
     createdAt: string;
 
