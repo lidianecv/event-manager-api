efae90d4ecfdb915073f3242a7a4a22193ac29ec
Lidiane Nunes <lidiane2002nunes@gmail.com>
2025-08-26 12:33:11 -0100

    Criei endpoints do users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private userService:UsersService ){}
+
+    @Get('me')
+    getMyUser(){
+        return this.userService
+    }
+
+    @Get('/id/:id')
+     findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService
     
+  }
+
+
 }
+
+
+
+// GET /users/me → Obter perfil do usuário logado
+// GET /users/:id → Obter perfil público de um usuário (eventos criados, participando)
